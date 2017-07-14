6df6d2b4cc83a8261e555c8e0baf30745b9044a8
menno doeterniettoe <mennowo@gmail.com>
2017-07-14 17:02:53 +0200

    Added precision timers

---
+++ CodingConnected.TLCProF/Generic/PosixHiPrecTimer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Mono.Unix.Native;
+
+namespace CodingConnected.TLCProF.Generic
+{
+    // From: https://stackoverflow.com/questions/37814505/mono-high-resolution-timer-on-linux
+    public class PosixHiPrecTimer
+    {
+        #region Fields
+
+        private const uint SafeDelay = 0; // millisecond (for slightly early wakeup)
+        private readonly Stopwatch _watch = new Stopwatch();
+        private bool _enabled;
+        private Timespec _pendingNanosleepParams;
+        private Timespec _threadNanosleepParams;
+        private readonly object _lockObject = new object();
+
+        #endregion // Fields
+
+        #region Properties
+
+        internal bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                if (value && !_enabled)
+                {
+                    _watch.Start();
+                    _enabled = true;
+                    Task.Run(TickGenerator); // fire up new thread
+                }
+                else
+                {
+                    lock (_lockObject)
+                    {
+                        _enabled = false;
+                    }
+                }
+            }
+        }
+
+        public long Interval
+        {
+            get
+            {
+                double totalNanoseconds;
+                lock (_lockObject)
+                {
+                    totalNanoseconds = 1e9 * _pendingNanosleepParams.tv_sec + _pendingNanosleepParams.tv_nsec;
+                }
+                return (int)(totalNanoseconds * 1e-6); // return value in ms
+            }
+            set
+            {
+                lock (_lockObject)
+                {
+                    _pendingNanosleepParams.tv_sec = value / 1000;
+                    _pendingNanosleepParams.tv_nsec = (long)((value % 1000) * 1e6); //set value in ns
+                }
+            }
+        }
+
+        #endregion // Properties
+
+        #region Events
+
+        public event EventHandler Tick; // Tick event 
+
+        #endregion // Events
+
+        #region Private Methods
+
+        private Task TickGenerator()
+        {
+            while (true)
+            {
+                // Check if thread has been told to halt
+                lock (_lockObject)
+                {
+                    if (!_enabled) break;
+                }
+                var curTime = _watch.ElapsedMilliseconds;
+                if (curTime >= Interval)
+                {
+                    _watch.Restart();
+                    Tick?.Invoke(this, new EventArgs());
+                }
+                else
+                {
+                    var iTimeLeft = (Interval - curTime); // How long to delay for 
+                    if (iTimeLeft < SafeDelay) continue;
+                    // Task.Delay has resolution 15ms//await Task.Delay(TimeSpan.FromMilliseconds(iTimeLeft - safeDelay));
+                    _threadNanosleepParams.tv_nsec = (int)((iTimeLeft - SafeDelay) * 1e6);
+                    _threadNanosleepParams.tv_sec = 0;
+                    Syscall.nanosleep(ref _threadNanosleepParams, ref _threadNanosleepParams);
+                }
+
+            }
+            _watch.Stop();
+            return null;
+        }
+
+        #endregion // Private Methods
+    }
+}
--- CodingConnected.TLCProF/Hosting/SimpleControllerHost.cs
+++ CodingConnected.TLCProF/Hosting/SimpleControllerHost.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
+using CodingConnected.TLCProF.Generic;
 using CodingConnected.TLCProF.Management;
 using CodingConnected.TLCProF.Models;
 using CodingConnected.TLCProF.Simulation;
@@ -15,14 +20,20 @@ namespace CodingConnected.TLCProF.Hosting
         #region Fields
 
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private static readonly bool IsPosixEnvironment = Path.DirectorySeparatorChar == '/';
 
         private readonly int _stepsize;
+        private readonly int _stepsizemargin;
         private readonly ControllerManager _manager;
 
         private bool _stepdelay;
         private DateTime _previousTime;
         private bool _running;
         private Thread _runThread;
+
+        private WinHiPrecTimer _winCycleTimer;
+        private PosixHiPrecTimer _linCycleTimer;
+        private Stopwatch watch;
 
         #endregion // Fields
 
@@ -57,8 +68,92 @@ namespace CodingConnected.TLCProF.Hosting
         public void StartController()
         {
             _running = true;
-            _runThread = new Thread(RunController);
-            _runThread.Start();
+            //_runThread = new Thread(RunController);
+            //_runThread.Start();
+            watch = new Stopwatch();
+            if (IsPosixEnvironment)
+            {
+                _linCycleTimer = new PosixHiPrecTimer();
+                _linCycleTimer.Tick += LinCycleTimerElapsed;
+                _linCycleTimer.Interval = _stepsize;
+                _linCycleTimer.Enabled = true;
+            }
+            else
+            {
+                _winCycleTimer = new WinHiPrecTimer();
+                _winCycleTimer.Elapsed += WinCycleTimerElapsed;
+                _winCycleTimer.Interval = _stepsize;
+                _winCycleTimer.Resolution = 25;
+                _winCycleTimer.Start();
+            }
+
+        }
+
+        private void WinCycleTimerElapsed(object sender, EventArgs eventArgs)
+        {
+            var elapsed = watch.ElapsedMilliseconds;
+            watch.Reset();
+            watch.Start();
+
+            if (elapsed > _stepsize * 2)
+            {
+                _logger.Warn("Control loop cycle took longer than twice the desired step size: {0} ms", elapsed);
+            }
+
+            Simulator?.SimulationStep(elapsed);
+            _manager.ExecuteStep(elapsed);
+            StepTaken?.Invoke(this, EventArgs.Empty);
+
+        }
+
+        private void LinCycleTimerElapsed(object sender, EventArgs eventArgs)
+        {
+            var elapsed = watch.ElapsedMilliseconds;
+            watch.Reset();
+            watch.Start();
+
+            if (elapsed > _stepsizemargin)
+            {
+                _logger.Warn("Control loop cycle took than 1.5 x the desired step size: {0} ms", elapsed);
+            }
+
+            Simulator?.SimulationStep(elapsed);
+            _manager.ExecuteStep(elapsed);
+            StepTaken?.Invoke(this, EventArgs.Empty);
+        }
+
+        [UsedImplicitly]
+        public void StopController()
+        {
+            //if (_runThread == null)
+            //{
+            //    _logger.Error("StopController was called without a prior call to StartController.");
+            //    return;
+            //}
+            if (IsPosixEnvironment)
+            {
+                if (_linCycleTimer == null)
+                {
+                    _logger.Error("StopController was called without a prior call to StartController.");
+                    return;
+                }
+                _linCycleTimer.Enabled = false;
+                _linCycleTimer = null;
+            }
+            else
+            {
+                if (_winCycleTimer == null)
+                {
+                    _logger.Error("StopController was called without a prior call to StartController.");
+                    return;
+                }  
+                _winCycleTimer.Stop();
+                _winCycleTimer.Dispose();
+                _winCycleTimer = null;
+            }
+
+            //_runThread.Interrupt();
+            _running = false;
         }
 
         #endregion // Public methods
@@ -71,14 +166,46 @@ namespace CodingConnected.TLCProF.Hosting
             _logger.Info("RunController started.");
             try
             {
-                _previousTime = DateTime.Now;
-                double elapsed, del;
+                //_previousTime = DateTime.Now;
+                //double elapsed, del;
+                //while (true)
+                //{
+                //    if (_stepdelay)
+                //    {
+                //        elapsed = DateTime.Now.Subtract(_previousTime).TotalMilliseconds;
+                //        _previousTime = DateTime.Now;
+                //        if (elapsed > _stepsize * 2)
+                //        {
+                //            _logger.Warn("Control loop cycle took longer than twice the desired step size: {0} ms", elapsed);
+                //        }
+                //
+                //        Simulator?.SimulationStep(elapsed);
+                //        _manager.ExecuteStep(elapsed);
+                //        StepTaken?.Invoke(this, EventArgs.Empty);
+                //
+                //        del = _stepsize - elapsed;
+                //        if (del > 0)
+                //        {
+                //            Thread.Sleep((int)del);
+                //        }
+                //    }
+                //    else
+                //    {
+                //        Simulator?.SimulationStep(_stepsize);
+                //        _manager.ExecuteStep(_stepsize);
+                //        StepTaken?.Invoke(this, EventArgs.Empty);
+                //    }
+                //}
+                long elapsed = 0;
+                var watch = new Stopwatch();
                 while (true)
                 {
                     if (_stepdelay)
                     {
-                        elapsed = DateTime.Now.Subtract(_previousTime).TotalMilliseconds;
-                        _previousTime = DateTime.Now;
+                        elapsed = watch.ElapsedMilliseconds;
+                        watch.Reset();
+                        watch.Start();
+
                         if (elapsed > _stepsize * 2)
                         {
                             _logger.Warn("Control loop cycle took longer than twice the desired step size: {0} ms", elapsed);
@@ -88,7 +215,7 @@ namespace CodingConnected.TLCProF.Hosting
                         _manager.ExecuteStep(elapsed);
                         StepTaken?.Invoke(this, EventArgs.Empty);
 
-                        del = _stepsize - elapsed;
+                        var del = _stepsize - watch.ElapsedMilliseconds;
                         if (del > 0)
                         {
                             Thread.Sleep((int) del);
@@ -112,31 +239,6 @@ namespace CodingConnected.TLCProF.Hosting
             }
         }
 
-        [UsedImplicitly]
-        public void StopController()
-        {
-            if (_runThread == null)
-            {
-                _logger.Error("StopController was called without a prior call to StartController.");
-                return;
-            }
-
-            _runThread.Interrupt();
-
-            // Bring controller to allred state
-            Task.Run(() =>
-            {
-                while (
-                    _manager.Controller.ControllerState == ControllerStateEnum.AllRed &&
-                    _manager.Controller.SignalGroups.Any(x => x.State != SignalGroupStateEnum.Red))
-                    {
-                        _manager.ExecuteStep(_stepsize);
-                    }
-            });
-
-            _running = false;
-        }
-
         #endregion // Private methods
 
         #region Constructor
@@ -146,6 +248,7 @@ namespace CodingConnected.TLCProF.Hosting
             _manager = manager;
             Simulator = simulator;
             _stepsize = stepsize;
+            _stepsizemargin = (int) (stepsize * 1.5);
             _stepdelay = stepdelay;
         }
 
